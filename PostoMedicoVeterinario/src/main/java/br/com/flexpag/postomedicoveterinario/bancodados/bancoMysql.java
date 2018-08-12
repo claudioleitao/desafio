bbd37607c6e82d72b98a126f5454d989d6731f77
claudioleitao <claudio.leitao@yahoo.com.br>
2018-08-12 02:56:19 +0100

    Mais uma alteração no projeto

---
+++ PostoMedicoVeterinario/src/main/java/br/com/flexpag/postomedicoveterinario/bancodados/bancoMysql.java
@@ -0,0 +1,15 @@
+package br.com.flexpag.postomedicoveterinario.bancodados;
+
+public class bancoMysql {
+	public static void conectar() {
+		
+	}
+	
+	public static void executar(String query) {
+		
+	}
+	
+	public static void desconectar() {
+		
+	}
+}
